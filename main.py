6205287eeaafbf022f107652ef8a21b67368f28d
Jeremy rist <rist.jeremy@gmail.com>
2023-11-26 11:25:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import rich
+from rich.prompt import Prompt
 
 def mainloop() -> None:
 	while True:
-		break
+		user_input = Prompt.ask(">",default="quickhelp")
+		if user_input == "quit":
+			break
+		elif user_input == "quickhelp":
+			rich.print("Quick Help not implemented.")
+		else:
+			rich.print("Command not recognized...")
+	rich.print("Bye!")
 
 if __name__ == "__main__":
 	mainloop()
